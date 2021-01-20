af911ac5b918dc6f42c1749f55d033c0338d2d1c
Yunus Emre KILIÇ <yunsmrek@gmail.com>
2021-01-20 23:50:30 +0300

    20 Ocak 2021 Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>//T tip int string class her şey olabiliyor
+    {
+        T[] items;// classın içinde her yer erişebilsin diye
+        public MyList()//ctor tab tab constructor bir class new lendiği zaman çalışan blok a denir
+        {
+            items = new T[0];//otomatik çalışıyor
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;//Geçici bir diziye elemanları atıyoruz
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length-1] = item;//0dan başladığı için -1 yazıyoruz
+
+        }
+    }
+}
