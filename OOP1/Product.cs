a8d1e6b13f76de4298056757db0fe8f4a3194fef
Yunus Emre KILIÇ <yunsmrek@gmail.com>
2021-01-20 22:30:18 +0300

    20 Ocak 2021 Ders Arası

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product//entities=varlık
+                 // bu tip classlarda sadece özellik olur
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }//referans anahtarlarını 2. sıraya yazarız.
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        // CRUD create read uptade read
+
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager// ya da ProductService
+        //ürünle ilgili operasyonlar burda yer alır
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+        
+    }
+}
